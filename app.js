b5ec5951b1883e54407618f62df1a69f7e453f01
Diptom Saha <sahadiptom128@gmail.com>
2020-10-14 12:45:38 -0700

    Api completed

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const Article = require('./model/article')
 const authRoutes = require('./routes/authRoutes');
 const meetingRoutes=require('./routes/meetingRoutes');
 const cookieParser = require('cookie-parser');
+const methodOverride = require('method-override')
 const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 const app= express();
 app.use(express.static('public'));
@@ -11,7 +13,8 @@ app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
-
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 const dbURI = 'mongodb+srv://dip128:1234@cluster0.iljra.mongodb.net/node-auth?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
@@ -26,8 +29,10 @@ app.get('/',(req,res)=>{
 });  
 
 app.get('/add', requireAuth ,(req,res)=>res.send('add'))
-app.use('/meeting', meetingRoutes)
+app.use('/meeting',requireAuth, meetingRoutes)
+app.use('/articles', requireAuth, meetingRoutes)
 app.use(authRoutes);
+
 app.use((req,res)=>{
     res.status(404).render('404');
 })
